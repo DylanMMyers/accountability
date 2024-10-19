d7f1f80fa6bbdc7def149f7f801a0956bba60b18
walk3rc <61813096+walk3rc@users.noreply.github.com>
2024-10-19 14:44:06 -0500

    Added file command to take url and return file location to transcript

---
+++ yt/GetTranscriptFromYT.py
@@ -0,0 +1,45 @@
+import yt_dlp
+from deepgram import Deepgram
+import asyncio, os
+
+def returnTranscriptFilePathFromURL(url):
+    ################ DOWNLOAD VIDEO ##################
+    url = "https://www.youtube.com/watch?v=NP96mMBxI2E&ab_channel=C-SPAN"
+
+    ydl_opts = {
+    'format': 'bestaudio/best',
+    'outtmpl': './yt/audOut/%(title)s.mp3'
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info_dict = ydl.extract_info(url, download=False)
+        videoTitle = info_dict.get('title', None)
+        ydl.download(url)
+
+    VIDEO_FILE = ".\\yt\\audOut\\" + videoTitle + ".mp3"
+
+    ########## CONVERTING VIDEO TO TRANSCRIPT (JSON) ##################
+
+    DEEPGRAM_API_KEY = 'd39e79e5d1cecef3561049b576e5135cc0b49234' 
+    PARAMS = {'punctuate': True, 'tier': 'enhanced', 'detect_entities' : True}
+    transcription_file = ".\\yt\\jsonOut\\" + videoTitle + '.txt'
+    async def main(): 
+        deepgram = Deepgram(DEEPGRAM_API_KEY)
+        print("Currently transcribing ", VIDEO_FILE) 
+        
+        with open(VIDEO_FILE, 'rb') as audio: 
+            source = {'buffer': audio, 'mimetype': 'audio/mp3'} 
+            response = await deepgram.transcription.prerecorded(source, PARAMS) 
+            transcript = response["results"]["channels"][0]["alternatives"][0]["transcript"] # Output the transcript print("Transcript:", transcript)
+            print("Transcript:\n"+transcript)
+            
+            os.remove(VIDEO_FILE) # Delete video
+            
+            
+            
+            with open(transcription_file, "w") as outfile: 
+                outfile.write(transcript)
+
+    asyncio.run(main())
+    return transcription_file
+
