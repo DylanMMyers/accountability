ef1bda6d6b7d5bda3b7cf8b37dfdb7e33d83240d
walk3rc <61813096+walk3rc@users.noreply.github.com>
2024-10-20 01:24:19 -0500

    new transcript thing

--- dj_app/scripts/urlToResponse.py
+++ dj_app/scripts/urlToResponse.py
@@ -1,40 +1,40 @@
-import yt_dlp
-from deepgram import Deepgram
-import asyncio, os
+# import yt_dlp
+# from deepgram import Deepgram
+# import asyncio, os
 
-def returnTranscriptFilePathFromURL(url):
-    ################ DOWNLOAD VIDEO ##################
-    ydl_opts = {
-    'format': 'bestaudio/best',
-    'outtmpl': './dj_app/scripts/audOut/%(title)s.mp3'
-    }
+# def returnTranscriptFilePathFromURL(url):
+#     ################ DOWNLOAD VIDEO ##################
+#     ydl_opts = {
+#     'format': 'bestaudio/best',
+#     'outtmpl': './dj_app/scripts/audOut/%(title)s.mp3'
+#     }
 
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        info_dict = ydl.extract_info(url, download=False)
-        videoTitle = info_dict.get('title', None)
-        ydl.download(url)
+#     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+#         info_dict = ydl.extract_info(url, download=False)
+#         videoTitle = info_dict.get('title', None)
+#         ydl.download(url)
 
-    VIDEO_FILE = ".\\dj_app\\scripts\\audOut\\" + videoTitle + ".mp3"
+#     VIDEO_FILE = ".\\dj_app\\scripts\\audOut\\" + videoTitle + ".mp3"
 
-    ########## CONVERTING VIDEO TO TRANSCRIPT (JSON) ##################
+#     ########## CONVERTING VIDEO TO TRANSCRIPT (JSON) ##################
 
-    DEEPGRAM_API_KEY = 'd39e79e5d1cecef3561049b576e5135cc0b49234' 
-    PARAMS = {'punctuate': True, 'tier': 'enhanced', 'detect_entities' : True}
-    transcription_file = ".\\dj_app\\scripts\\jsonOut\\" + videoTitle + '.txt'
-    async def main(): 
-        deepgram = Deepgram(DEEPGRAM_API_KEY)
-        with open(VIDEO_FILE, 'rb') as audio: 
-            source = {'buffer': audio, 'mimetype': 'audio/mp3'} 
-            response = await deepgram.transcription.prerecorded(source, PARAMS) 
-            transcript = response["results"]["channels"][0]["alternatives"][0]["transcript"] # Output the transcript print("Transcript:", transcript)
+#     DEEPGRAM_API_KEY = 'd39e79e5d1cecef3561049b576e5135cc0b49234' 
+#     PARAMS = {'punctuate': True, 'tier': 'enhanced', 'detect_entities' : True}
+#     transcription_file = ".\\dj_app\\scripts\\jsonOut\\" + videoTitle + '.txt'
+#     async def main(): 
+#         deepgram = Deepgram(DEEPGRAM_API_KEY)
+#         with open(VIDEO_FILE, 'rb') as audio: 
+#             source = {'buffer': audio, 'mimetype': 'audio/mp3'} 
+#             response = await deepgram.transcription.prerecorded(source, PARAMS) 
+#             transcript = response["results"]["channels"][0]["alternatives"][0]["transcript"] # Output the transcript print("Transcript:", transcript)
             
-            os.remove(VIDEO_FILE) # Delete video
+#             os.remove(VIDEO_FILE) # Delete video
             
-            with open(transcription_file, "w") as outfile: 
-                outfile.write(transcript)
+#             with open(transcription_file, "w") as outfile: 
+#                 outfile.write(transcript)
 
-    asyncio.run(main())
-    return transcription_file
+#     asyncio.run(main())
+#     return transcription_file
 
 from openai import OpenAI
 
@@ -52,7 +52,7 @@ def query(transcript, title):
     - response (str): The API's response containing the assessment.
     """
     prompt = f"""
-    You are about to receive a transcript from some political event, like a speech or debate. Identify which political figures are speaking and assess the accuracy and truth of their statement. For each political figure you identify, rate them from 1-5 on how accurate and truthful their statements were. Provide a summary for each political figure and what they said, how truthful certain statements were and provide further context or explanations of how what they said is accurate, deceptive, or a lie. Avoid categorizing statements similar to “I want to be a good president” and such general statements. Please also state what the most deceptive statement was. When selecting sources try to use a mix of typically conservative and liberal sources to provide a wider world view on the situation. Based on these sources, provide a confidence score out of 100 for how accurate your assessment is of each candidate, with 100 being fully confident. Please cite these sources at the bottom of the response. The transcript has the title “{title}”, which may help provide you with context. PLEASE DO NOT PROVIDE A TABLE OR ANY OTHER CONTEXT BEFORE THE OUTPUT, JUST PROVIDE THE DATA IN THE FORMAT BELOW:
+    You are about to receive a transcript from some political event, like a speech or debate. Identify which political figures are speaking and assess the accuracy and truth of their statement. For each political figure you identify, rate them from 1-5 on how accurate and truthful their statements were. Provide a summary for each political figure and what they said, how truthful certain statements were and provide further context or explanations of how what they said is accurate, deceptive, or a lie. Avoid categorizing statements similar to “I want to be a good president” and such general statements. Please also state what the most deceptive statement was. When selecting sources try to use a mix of typically conservative and liberal sources to provide a wider world view on the situation. Based on these sources, provide a confidence score out of 100 for how accurate your assessment is of each candidate, with 100 being fully confident. Please cite these sources at the bottom of the response. The transcript has the link “{title}”, which may help provide you with context. PLEASE DO NOT PROVIDE A TABLE OR ANY OTHER CONTEXT BEFORE THE OUTPUT, JUST PROVIDE THE DATA IN THE FORMAT BELOW:
 
     A response may look like:
     (Political Figure) : (rating/5):
@@ -91,14 +91,29 @@ def query(transcript, title):
     #print(result_text)
     return result_text
 
+from youtube_transcript_api import YouTubeTranscriptApi
+def getTranscript(url):
+    try:
+        video_id = url.remove("https://www.youtube.com/watch?v=","")[:15]
+        transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
+        transcript = transcript_list.find_transcript(['en'])
+        totalTranscript = ""
+        for item in transcript.fetch():
+            totalTranscript += item['text'] + " "
+        return totalTranscript
+    except:
+        print("Something went wrong with getting transcript!")
+
 
 import os
 
+
 def mainf(url):
-    loc = returnTranscriptFilePathFromURL(url)
-    title = loc.replace(".\\dj_app\\scripts\\jsonOut\\", "").replace(".txt","")
-    with open(loc, 'r', encoding='utf-8') as file:
-        transcript = file.read()
-    response = query(transcript, title)
+    #loc = returnTranscriptFilePathFromURL(url)
+    #title = loc.replace(".\\dj_app\\scripts\\jsonOut\\", "").replace(".txt","")
+    # with open(loc, 'r', encoding='utf-8') as file:
+    #     transcript = file.read()
+    transcript = getTranscript(url)
+    response = query(transcript, url)
     return response
 
